104b59d61068bb07431a1c9c18467d889f58ece7
Asher-Ul-Haque <contactasher.u.haque@gmail.com>
2026-08-11 11:58:09 +0530

    Removed .build from git tracking

--- .build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/asher/Documents/ForgeLibrary
-# Build directory: /home/asher/Documents/ForgeLibrary/.build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
-subdirs("examples")
